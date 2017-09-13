db56c9039e2b0dafe445f9acd998b59fbc9c7611
Joseph Lawrence Eklund <eklund7@llnl.gov>
2017-09-13 11:40:14 -0400

    Removed old archiveadapter abstract. Created httpadapter abstract.

--- maestrowf/abstracts/interfaces/httpadapter.py
+++ maestrowf/abstracts/interfaces/httpadapter.py
@@ -4,7 +4,7 @@ HTTP Adapter Interface for defining the API for interacting with HTTP requests.
 from abc import ABCMeta, abstractmethod
 import logging
 import six
-
+import json
 
 LOGGER = logging.getLogger(__name__)
 
@@ -14,7 +14,29 @@ class HttpAdapter(object):
     Abstract class representing the interface for interacting with HTTP.
     """
     def _get(self, url):
-        pass
+        """
+        Executes a HTTP Get on the given URL.
+
+        :param url: The url to perform the HTTP Get on.
+        :returns: A tuple containing the HTTP response and content.
+        """
+        LOGGER.debug('Performing HTTPGet on: {}'.format(url))
+        headers = {'Accept': 'application/json'}
+        response, content = self.client.request(url, headers=headers)
+        return response, content
 
     def _post(self, url, content):
-        pass
+        """
+        Executes a HTTP Post on the given URL.
+
+        :param url: The url to perform the HTTP Get on.
+        :param content: The content to POST with.
+        :returns: A tuple containing the HTTP response and content.
+        """
+        LOGGER.debug('Performing HTTPPost on: {}\nwith content: {}'\
+            ''.format(url, content))
+        headers = {'Content-Type': 'application/json'}
+        response, content = self.client.request(url, 'POST',
+                                                json.dumps(content),
+                                                headers=headers)
+        return response, content
--- maestrowf/interfaces/archive/corrarchiveadapter.py
+++ maestrowf/interfaces/archive/corrarchiveadapter.py
@@ -9,12 +9,12 @@ import logging
 import httplib2
 import json
 
-from maestrowf.abstracts.interfaces.archiveadapter import ArchiveAdapter
+from maestrowf.abstracts.interfaces.httpadapter import HttpAdapter
 from maestrowf.interfaces.archive import utils
 
 LOGGER = logging.getLogger(__name__)
 
-class CorrHttpAdapter(ArchiveAdapter):
+class CorrHttpAdapter(HttpAdapter):
     """
     Adatper for sending Maestro specification data to CoRR. This implementation
     is based on:
@@ -37,34 +37,6 @@ class CorrHttpAdapter(ArchiveAdapter):
                 path, key, token)
         self.client = httplib2.Http('.cache',
             disable_ssl_certificate_validation=disable_ssl_certificate_validation)
-
-    def _get(self, url):
-        """
-        Executes a HTTP Get on the given URL.
-
-        :param url: The url to perform the HTTP Get on.
-        :returns: A tuple containing the HTTP response and content.
-        """
-        LOGGER.debug('Performing HTTPGet on: {}'.format(url))
-        headers = {'Accept': 'application/json'}
-        response, content = self.client.request(url, headers=headers)
-        return response, content
-
-    def _post(self, url, content):
-        """
-        Executes a HTTP Post on the given URL.
-
-        :param url: The url to perform the HTTP Get on.
-        :param content: The content to POST with.
-        :returns: A tuple containing the HTTP response and content.
-        """
-        LOGGER.debug('Performing HTTPPost on: {}\nwith content: {}'\
-            ''.format(url, content))
-        headers = {'Content-Type': 'application/json'}
-        response, content = self.client.request(url, 'POST',
-                                                json.dumps(content),
-                                                headers=headers)
-        return response, content
 
     def create_project(self, spec_path):
         """
