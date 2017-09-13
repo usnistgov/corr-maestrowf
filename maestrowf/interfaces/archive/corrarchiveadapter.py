840b9d5f7bfcb92f399d21eda9602a1b380c297e
Joseph Lawrence Eklund <eklund7@llnl.gov>
2017-09-13 11:10:59 -0400

    Added ability to update projects and records.

--- maestrowf/interfaces/archive/corrarchiveadapter.py
+++ maestrowf/interfaces/archive/corrarchiveadapter.py
@@ -68,41 +68,62 @@ class CorrHttpAdapter(ArchiveAdapter):
 
     def create_project(self, spec_path):
         """
-        Will attempt to create a new project in CoRR with the given
-        Maestro specificiation path.
+        Creates a new project in CoRR with the given Maestro
+        specificiation path.
 
-        :param spec_path: The maestro spec_path to use for the new project.
+        :param spec_path: The maestro spec path to use for the new project.
         :returns: A tuple containing the HTTP response and content.
         """
         LOGGER.debug('Creating a new project with spec at: {}'\
             ''.format(spec_path))
-
         # Extract the yaml data
         yaml_data = utils.read_yaml(spec_path)
         _content = {}
         _content['name'] = yaml_data['description']['name']
         _content['description'] = yaml_data['description']['description']
-        url = "%sproject/create" % (self.server_url)
+        url = '{}project/create'.format(self.server_url)
 
         response, content = self._post(url, _content)
         self._check_response_content(url=url, response=response,
-                        content=content, response_code=200, content_code=201)
+                    content=content, response_code=200, content_code=201)
+        return response, content
 
     def update_project(self, spec_path):
         """
-        Update a project in CoRR with the given Maestro specificiation path.
+        Updates a project in CoRR with the given Maestro specificiation path.
 
-        :param spec_path: The spec_path to use for the project.
+        :param spec_path: The maestro spec path to use for the project.
         :returns: A tuple containing the HTTP response and content.
         """
-        raise NotImplementedError
+        LOGGER.debug('Updating project with spec at: {}'\
+            ''.format(spec_path))
+        # Extract the yaml data
+        yaml_data = utils.read_yaml(spec_path)
+        _content = {}
+        _content['name'] = yaml_data['description']['name']
+        _content['description'] = yaml_data['description']['description']
+
+        # Check if project exists to update
+        project = self.has_project(_content['name'])
+        if not project:
+            msg = ('No project exists with the name: {}'\
+                ''.format(_content['name']))
+            LOGGER.exception(msg)
+            raise ValueError(msg)
+
+        # Update the project
+        url = '{}project/update/{}'.format(self.server_url, project['id'])
+        response, content = self._post(url, _content)
+        self._check_response_content(url=url, response=response,
+                    content=content, response_code=200, content_code=201)
+        return response, content
 
     def has_project(self, project_name):
         """
         Check to see if an existing project exists with the given project name.
 
         :param project_name: The project name to check for.
-        :returns: True if the project exists, False otherwise.
+        :returns: The project if it exists, None if it doesn't.
         """
         LOGGER.debug('Checking if project <{}> exists.'.format(project_name))
         project = None
@@ -115,46 +136,54 @@ class CorrHttpAdapter(ArchiveAdapter):
             if p['name'] == project_name:
                 project = p
                 break
-        if project:
-            return True
-        else:
-            return False
+        return project
 
     def create_record(self, project_name, corr_spec_path):
         """
-        Create the record to the corresponding project name in CoRR. If the
+        Create a record with the corresponding project name in CoRR. If the
         project does not exist, will create the project and then insert the
         record.
 
         :param project_name: The project name to use for the record.
-        :param corr_spec_path: The path to the CoRR spec path to store.
+        :param corr_spec_path: The path to the CoRR spec to store.
         :returns: A tuple containing the HTTP response and content.
         """
-        LOGGER.debug('Inserting new record into project: {}'\
-            ''.format(project_name))
-        record_id = None
+        LOGGER.debug('Inserting new record into project: {}\n'\
+            'with spec: {}'.format(project_name, corr_spec_path))
         project = None
-        url = "%sprojects" % (self.server_url)
-        response, content = self._get(url)
-        self._check_response_content(url=url, response=response,
-                        content=content, response_code=200, content_code=200)
-        result = json.loads(content)
-        for p in result['content']['projects']:
-            if p['name'] == project_name:
-                project = p
-                break
-        if project is None:
+        project = self.has_project(project_name=project_name)
+        if not project:
             LOGGER.debug('Project <{}> does not exist. Creating new project.'\
                 ''.format(project_name))
             response, content = self.create_project(project_name)
             project = json.loads(content)['content']
 
+        # Create the record
         record = utils.read_yaml(corr_spec_path)
-        url = "%sproject/record/create/%s" % (self.server_url, project['id'])
-        headers = {'Content-Type': 'application/json'}
+        url = '{}project/record/create/{}'.format(self.server_url,
+            project['id'])
         response, content = self._post(url, record)
         self._check_response_content(url=url, response=response,
                         content=content, response_code=200, content_code=201)
+        return response, content
+
+    def update_record(self, project_name, record_id, corr_spec_path):
+        """
+        Update the record with the corresponding project name and record id in
+        CoRR.
+
+        :param project_name: The project name to use for the record update.
+        :param corr_spec_path: The path to the CoRR spec to store.
+        :returns: A tuple containing the HTTP response and content.
+        """
+        LOGGER.debug('Updating record: <{}>\n of project: {}\n'\
+           'with spec: {}'.format(record_id, project_name, corr_spec_path))
+        record = utils.read_yaml(corr_spec_path)
+        url = "%srecord/update/%s" % (self.server_url, record_id)
+        response, content = self._post(url, record)
+        self._check_response_content(url=url, response=response,
+                        content=content, response_code=200, content_code=201)
+        record_payload = json.loads(content)
         return response, content
 
     def _check_response_content(cls, url, response, content, response_code=200,
@@ -170,17 +199,21 @@ class CorrHttpAdapter(ArchiveAdapter):
             Default 200.
         :param content_code: The content code to check content against. Default
             200.
+        :raises StandardError: If the response status does not match the
+            inputed response_code or if the content code does not match the
+            inputed content_code.
         """
         LOGGER.debug('Checking response and content codes.')
         if response.status != response_code:
-            msg = ('URL <{}> provided bad response status: {}\nContent: {}'\
+            msg = ('URL <{}>\nProvided bad response status: {}\nContent: {}'\
                 ''.format(url, response.status, content))
             LOGGER.exception(msg)
-            raise StandardError(msg)
+            raise ValueError(msg)
         else:
             code = json.loads(content)['code']
             if code != content_code:
-                msg = ('URL <{}> provided bad content code status: {}\n'\
-                    'Content: {}'.format(response.status, content))
+                msg = ('URL <{}>\nProvided bad content code status: {}\n'\
+                    'Expected content code: {}\nContent: {}'.format(url, code,
+                        content_code, content))
                 LOGGER.exception(msg)
-                raise StandardError(msg)
+                raise ValueError(msg)
